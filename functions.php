21dc3aff23565558955a9d324b72c3d043b6ee85
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-01-30 11:41:01 +0300

    Функции

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Функции и объявления темы Impact.
+ * 
+ * Тема Journal.
+ * 
+ * @copyright Copyright (c) 2015-2024 GearMagic Web-студия
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
